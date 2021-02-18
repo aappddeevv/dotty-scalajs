7e1642d1fdb1ec9db5187694214b609b87605210
Martin Mauch <martin.mauch@gmail.com>
2021-02-18 18:58:27 +0100

    Update dependencies and code

--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,3 +1,3 @@
-addSbtPlugin("org.scala-js" % "sbt-scalajs" % "1.2.0")
-addSbtPlugin("ch.epfl.lamp" % "sbt-dotty" % "0.4.2")
+addSbtPlugin("org.scala-js" % "sbt-scalajs" % "1.5.0")
+addSbtPlugin("ch.epfl.lamp" % "sbt-dotty" % "0.5.2")
 
--- src/main/scala/fetch.scala
+++ src/main/scala/fetch.scala
@@ -11,10 +11,9 @@ trait Response extends js.Object:
     val ok: Boolean = js.native
     def text(): js.Promise[String] = js.native
 
-// This does not compile, yet...
-// @js.native
-// @JSImport("node-fetch", JSImport.Default)
-// def fetch2(url: String, arg: js.UndefOr[js.Dynamic]=js.undefined): js.Promise[Response] = js.native
+@js.native
+@JSImport("node-fetch", JSImport.Default)
+def fetch2(url: String, arg: js.UndefOr[js.Dynamic]=js.undefined): js.Promise[Response] = js.native
 
 // Traditional way.
 @js.native
@@ -23,7 +22,7 @@ object fetch extends js.Any:
     def apply(url: String, options: js.UndefOr[js.Dynamic]=js.undefined): js.Promise[Response] = js.native
 
 def fetchAndPrint(url: String) =
-    fetch(url).toFuture
+    fetch2(url).toFuture
     .flatMap:
         response =>
             if(!response.ok) Future.failed(new Exception("Not 200"))
